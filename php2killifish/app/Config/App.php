bbdbb6f2f8f262275b4efa6cd27e2bfc870be6dc
luca-bruno <56382257+luca-bruno@users.noreply.github.com>
2020-12-08 12:08:01 +0100

    Registration function implemented
    - Restructuring of database table and columns + data types (replaced .sql file)
    
    - Set correct paths and routing in .env, App.php & Routes.php
    
    - Added UserController (validation rules/handling, error handling, form helpers & session handling)
    
    - Added UserModel (linking session data to database, database field handling)
    
    - Added header.php & footer.php templates in Views/templates folder
    
    - Added mockup temporary login and register HTML pages to test out registration functionality
    
    - Added partial commenting to code sections previously unbeknownst to me

--- php2killifish/app/Config/App.php
+++ php2killifish/app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost:8080/';
+	public $baseURL = 'http://localhost/php2killifish/php2killifish/public';
 
 	/*
 	|--------------------------------------------------------------------------
